5c4c891ad871aac28a80edc5cd4af1ec14bc4564
Milosz <miloszloniewski02@gmail.com>
2023-04-02 19:51:54 +0200

    changed header height

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
 const burgerBtn = document.querySelector('.nav__menu');
-const nav = document.querySelector('.nav__items');
+const nav = document.querySelector('.nav');
+const navItems = document.querySelector('.nav__items');
 
 burgerBtn.addEventListener('click', () => {
-	nav.classList.toggle('nav__items--active');
+	navItems.classList.toggle('nav__items--active');
 	const burgerSrc = burgerBtn.getAttribute('src');
 	console.log(burgerSrc);
 	if (burgerSrc === './dist/img/icon-hamburger.svg') {
 		burgerBtn.setAttribute('src', './dist/img/icon-close.svg');
+		nav.style.position = 'fixed';
 	} else if (burgerSrc === './dist/img/icon-close.svg') {
 		burgerBtn.setAttribute('src', './dist/img/icon-hamburger.svg');
+		nav.style.position = 'absolute';
 	}
 });
